d8eac0ecf411d46b3f933d08730270488dcdba3f
Esther Weyinmi Isack <isack.weyinmi@gmail.com>
2019-03-05 00:46:18 +0100

    sample input updated in Pawn class - test passed.

--- src/PawnTest.java
+++ src/PawnTest.java
@@ -3,8 +3,6 @@ public class PawnTest extends junit.framework.TestCase {
     public void testCreate() {
         Pawn pawn = new Pawn("white");
         String pawn1Color = pawn.getPawnColor();
-
-
         assertEquals("white", pawn1Color);
 
 
